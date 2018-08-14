7227516d0f9565e9f6f7021e7e98ff3166c086d0
Emil Folino <emilfolino@gmail.com>
2018-08-14 13:41:53 +0200

    made it work like it is supposed to do

--- slideorama.js
+++ slideorama.js
@@ -6,6 +6,28 @@
     var slides = document.getElementById("slide-container").children;
     var numberOfSlides = slides.length;
 
+    document.addEventListener("keyup", keyPressed);
+
+    function keyPressed(key) {
+        if (key.code === "Enter" || key.code === "ArrowRight") {
+            counter++;
+        }
+
+        if (key.code === "ArrowLeft") {
+            counter--;
+        }
+
+        if (counter > numberOfSlides - 1) {
+            counter = 0;
+        }
+
+        if (counter < 0) {
+            counter = numberOfSlides - 1;
+        }
+
+        showSlide(counter);
+    }
+
     function showSlide(index) {
         presentation.innerHTML = slides[index].outerHTML;
     }
